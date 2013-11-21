57beb4db7dcedb6f90347dd1fc7937ac750408a9
ib222dp <ib222dp@users.noreply.github.com>
2013-11-21 19:38:03 +0100

    guesskod.js

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,27 +2,32 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
-	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var guess = function(number){
-		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
-		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+	//Initiering av variabler, slumpar tal mellan 1 och 100
+    	var count = 0;
+    	var secret = Math.floor(Math.random() * 101);
 
 
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
-	};
-	
+    var guess = function (number) {
+        //Räknar upp variabeln count med 1
+        count = count += 1;
+
+        console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret inifrån funktionen.
+        console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
+
+        //If-satser som returnerar en array vars värden varierar beroende på hur det inmatade talet stämmer överens med det hemliga talet
+        if (number === secret) {
+            return [true, "Det hemliga talet var " + secret + " och du behövde " + count + "gissningar för att hitta det."];
+        } if (number < 1 || number > 100) {
+            return [false, "Talet är utanför intervallet 1 - 100."];
+        } if(isNaN(number)){
+            return [false, "Mata in ett tal med siffror."];
+        }if (number < secret) {
+            return [false, "Det hemliga talet är högre!"];
+        } if (number > secret) {
+            return [false, "Det hemliga talet är lägre!"];
+        }
+    };
 	// ------------------------------------------------------------------------------
-
-
-
 	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
 	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
 	var input = document.querySelector("#number");
@@ -38,6 +43,5 @@ window.onload = function(){
 		if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
 			submit.disabled = true;
 		}
-	
 	});
 };
