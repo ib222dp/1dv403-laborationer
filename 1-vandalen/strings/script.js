c6740858647d8c7444c23db3e45b994b911eb753
ib222dp <ib222dp@users.noreply.github.com>
2013-11-24 13:43:13 +0100

    stringskod2.js

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -2,9 +2,10 @@
 
 window.onload = function(){
 
-	var convertString = function(str){
-		
-	 //Kastar undantag om sträng är tom (http://stackoverflow.com/questions/2381456/javascript-how-to-check-if-a-string-is-empty)
+ 	// I denna funktion ska du skriva koden för att hantera "spelet"
+    	var convertString = function (str) {
+
+        //Kastar undantag om sträng är tom (http://stackoverflow.com/questions/2381456/javascript-how-to-check-if-a-string-is-empty)
         if (str === "") {
             throw new Error("Du måste skriva in något i rutan.");
         }
@@ -15,30 +16,28 @@ window.onload = function(){
 
         //Ändrar karaktärer till versaler eller gemener och lägger till dem i arrayen strArray 
         //(http://stackoverflow.com/questions/1027224/how-can-i-test-if-a-letter-in-a-string-is-uppercase-or-lowercase-using-javascrip)
-        try {
-            for (i = 0; i < str.length; i++) {
-                var character = str[i];
-                if (character === character.toLowerCase()) {
-                    strArray.push(character.toUpperCase());
-                } if (character === character.toUpperCase()) {
-                    strArray.push(character.toLowerCase());
-                }
+
+        for (i = 0; i < str.length; i++) {
+            var character = str[i];
+            if (character === character.toLowerCase()) {
+                strArray.push(character.toUpperCase());
+                continue;
+            } if (character === character.toUpperCase()) {
+                strArray.push(character.toLowerCase());
+                continue;
             }
-
-            //Gör om arrayen strArray till strängen newStr
-            var newStr = strArray.join('');
-
-            //Byter ut alla A och a till #
-            //(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace?redirectlocale=en-US&redirectslug=JavaScript%2FReference%2FGlobal_Objects%2FString%2Freplace)
-            var re = /a/gi;
-            var repStr = newStr.replace(re, "#");
-
-            return repStr;
-
-        } catch (Error) {
-
         }
-	};
+
+        //Gör om arrayen strArray till strängen newStr
+        var newStr = strArray.join('');
+
+        //Byter ut alla A och a till #
+        //(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace?redirectlocale=en-US&redirectslug=JavaScript%2FReference%2FGlobal_Objects%2FString%2Freplace)
+        var re = /a/gi;
+        var repStr = newStr.replace(re, "#");
+
+        return repStr;
+    };
 	// ------------------------------------------------------------------------------
 	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
 	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
